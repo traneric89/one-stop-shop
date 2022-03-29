ffe9acefac9df9e5d703053053c781e003fcc148
optiX <55358375+traneric89@users.noreply.github.com>
2022-03-29 14:22:17 -0400

    Added mission statement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Homepage from "./components/Homepage";
+import Mission from "./components/Mission";
 
 const App = () => {
   return (
     <div>
       <Homepage />
+      <Mission />
     </div>
   );
 };
---
+++ src/components/Mission.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Mission = () => {
+  return (
+    <div>
+      <h1>Our Mission</h1>
+      <h2>
+        Create the best performing products using sustainable materials so we
+        can all enjoy the beautiful mountains our planet has to offer.
+      </h2>
+      <div className="missions">
+        <div className="mission">Durable</div>
+        <div className="mission">Sustainable</div>
+        <div className="mission">Admirable</div>
+      </div>
+    </div>
+  );
+};
+
+export default Mission;
