32e049cf0c7042d8c3375e0aedeb3a7615d52946
optiX <55358375+traneric89@users.noreply.github.com>
2022-04-05 00:42:28 -0400

    Added typewriter homepage styling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import Navbar from "./Navbar";
+import { Typewriter } from "react-simple-typewriter";
 
 const Homepage = () => {
   const [numItems, setNumItems] = useState(0);
@@ -9,6 +10,17 @@ const Homepage = () => {
   return (
     <div>
       <Navbar numItems={numItems} itemsCart={itemsCart} />
+      <p className="typewriter">
+        <Typewriter
+          loop
+          cursor
+          cursorStyle="|"
+          typeSpeed={70}
+          deleteSpeed={100}
+          delaySpeed={1500}
+          words={["COMMITTED.", "DISCIPLINED.", "FOCUSED."]}
+        />
+      </p>
       <img
         className="main-img"
         src={require("../images/background.avif")}
